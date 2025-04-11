fac6b3f0a2adbe61e86d3dbae7a91e1e5e9e48b0
Lourenço Carlos <reincedaniel@gmail.com>
2025-04-11 02:00:45 +0100

    refactor: smooch bug

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -19,7 +19,8 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import Smoochx from "smooch";
+import Smooch from 'smooch';
+
 import { useQuasar } from "quasar";
 defineOptions({
   name: "IndexPage",
@@ -159,7 +160,7 @@ const arrayOfOperation = [
     isActivo: true,
   },
 ];
-const Smooch = Smoochx ?? window.Smooch;
+
 const refresh = () => {
 
   // window.location.reload()
